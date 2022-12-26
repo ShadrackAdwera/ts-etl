aa338fa659561013d4571e89b0946ea1ec27e87d
ShadrackAdwera <adweshshaddie@gmail.com>
2022-12-26 16:06:02 +0300

    create user schema

--- auth/src/models/User.ts
+++ auth/src/models/User.ts
@@ -1,1 +1,20 @@
-//
+import { Document, Model, model, Schema } from 'mongoose';
+
+interface UserDoc extends Document {
+  googleId: string;
+  username: string;
+}
+
+interface UserModel extends Model<UserDoc> {
+  googleId: string;
+  username: string;
+}
+
+const userSchema = new Schema({
+  googleId: { type: String, required: true },
+  username: { type: String, required: true },
+});
+
+const User = model<UserDoc, UserModel>('user', userSchema);
+
+export { User };
