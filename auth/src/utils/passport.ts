9acd7a67fb3a9a08a2e411818de315a9a3ce8d63
ShadrackAdwera <adweshshaddie@gmail.com>
2022-12-26 17:01:15 +0300

    set up passport config file

---
+++ auth/src/utils/passport.ts
@@ -0,0 +1,39 @@
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import { User } from '../models/User';
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      callbackURL: '/auth/google/callback',
+      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      proxy: true,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const existingUser = await User.findOne({ googleId: profile.id });
+        if (existingUser) {
+          return done(null, existingUser);
+        }
+        const user = await new User({
+          googleId: profile.id,
+          displayName: profile.displayName,
+        }).save();
+        done(null, user);
+      } catch (err) {
+        done(err instanceof Error ? err : 'An error occured', undefined);
+      }
+    }
+  )
+);
