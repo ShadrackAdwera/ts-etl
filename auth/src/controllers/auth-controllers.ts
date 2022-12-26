0a1d46f6bca38a7d66d1ba36b280d6229a962686
ShadrackAdwera <adweshshaddie@gmail.com>
2022-12-26 17:02:24 +0300

    configure auth controllers

--- auth/src/controllers/auth-controllers.ts
+++ auth/src/controllers/auth-controllers.ts
@@ -1,1 +1,22 @@
-//
+import { HttpError } from '@adwesh/common';
+import { Request, Response, NextFunction } from 'express';
+
+const getCallback = (req: Request, res: Response, next: NextFunction) => {
+  return res.redirect('/');
+};
+
+const logout = (req: Request, res: Response, next: NextFunction) => {
+  req.logOut((err) => {
+    if (err)
+      return next(
+        new HttpError(err instanceof Error ? err.message : 'Logout error', 500)
+      );
+    res.redirect('/login');
+  });
+};
+
+const currentUser = (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({ user: req.user });
+};
+
+export { getCallback, logout, currentUser };
