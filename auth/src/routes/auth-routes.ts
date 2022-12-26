a27f0fa9a9041e72025939c096f4709a7fc19a6c
ShadrackAdwera <adweshshaddie@gmail.com>
2022-12-26 17:01:34 +0300

    configure auth routes

--- auth/src/routes/auth-routes.ts
+++ auth/src/routes/auth-routes.ts
@@ -1,1 +1,20 @@
-//
+import passport from 'passport';
+import { Router } from 'express';
+
+import {
+  getCallback,
+  logout,
+  currentUser,
+} from '../controllers/auth-controllers';
+
+const router = Router();
+
+router.get(
+  '/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+router.get('/google/callback', passport.authenticate('google'), getCallback);
+router.get('/logout', logout);
+router.get('/current-user', currentUser);
+
+export { router as authRouter };
