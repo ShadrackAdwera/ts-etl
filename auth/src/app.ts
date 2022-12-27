8876620afd08f3bfffbfdb0181f4a9ddde7c56e9
ShadrackAdwera <adweshshaddie@gmail.com>
2022-12-27 15:45:45 +0300

    configuration for express session

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
 import passport from 'passport';
-import cookieSession from 'cookie-session';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
 
 import './utils/passport';
 import { authRouter } from './routes/auth-routes';
@@ -9,10 +10,31 @@ import { HttpError } from '@adwesh/common';
 const app = express();
 
 app.use(express.json());
+//CORS
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, PUT, PATCH, POST, DELETE, GET'
+  );
+  next();
+});
+
 app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [process.env.COOKIE_KEY!],
+  session({
+    cookie: {
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    },
+    saveUninitialized: true,
+    secret: process.env.COOKIE_KEY!,
+    store: new MongoStore({
+      collectionName: 'session',
+      mongoUrl: process.env.MONGO_URI,
+    }),
   })
 );
 app.use(passport.initialize());
