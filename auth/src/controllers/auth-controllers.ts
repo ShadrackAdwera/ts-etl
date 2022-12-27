ef11438266c5306498a522df0cfe184c0760d4a2
ShadrackAdwera <adweshshaddie@gmail.com>
2022-12-27 15:46:22 +0300

    create get users controller and fix redirect url

--- auth/src/controllers/auth-controllers.ts
+++ auth/src/controllers/auth-controllers.ts
@@ -1,8 +1,9 @@
 import { HttpError } from '@adwesh/common';
 import { Request, Response, NextFunction } from 'express';
+import { User, UserDoc } from '../models/User';
 
 const getCallback = (req: Request, res: Response, next: NextFunction) => {
-  return res.redirect('/');
+  return res.redirect('http://localhost:3000');
 };
 
 const logout = (req: Request, res: Response, next: NextFunction) => {
@@ -19,4 +20,20 @@ const currentUser = (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({ user: req.user });
 };
 
-export { getCallback, logout, currentUser };
+const getUsers = async (req: Request, res: Response, next: NextFunction) => {
+  let foundUsers: { _id: string & UserDoc }[];
+
+  try {
+    foundUsers = await User.find({});
+  } catch (error) {
+    return next(
+      new HttpError(
+        error instanceof Error ? error.message : 'An error occured',
+        500
+      )
+    );
+  }
+  res.status(200).json({ count: foundUsers.length, users: foundUsers });
+};
+
+export { getCallback, logout, currentUser, getUsers };
