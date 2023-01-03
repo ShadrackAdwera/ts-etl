2303da51912053215ab32caed13dd9ca5e44db60
ShadrackAdwera <adweshshaddie@gmail.com>
2023-01-03 12:22:16 +0300

    extract data from csv

--- etl/src/controllers/etl-controllers.ts
+++ etl/src/controllers/etl-controllers.ts
@@ -1,6 +1,8 @@
 import { HttpError } from '@adwesh/common';
 import { Request, Response, NextFunction } from 'express';
 import { Types } from 'mongoose';
+import { parse } from 'csv-parse';
+
 import { Etl, EtlDoc } from '../models/Etl';
 
 const getUploadedDocs = async (
@@ -21,3 +23,38 @@ const getUploadedDocs = async (
   }
   res.status(200).json({ count: etls.length, etls });
 };
+
+const publishDataFromUploadedDoc = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  /**
+   * Get document from request body
+   * Extract data from document body
+   * Sanitize data
+   * Publish data extracted from doc, to be listened to by data service
+   * Request pre-signed URL from s3 to upload the file to S3
+   * Save the file in s3 using the pre-signed URL
+   * Save the url alongside the id of the creator in MongoDB
+   */
+  const csvData: string[][] = [];
+  if (!req.files || Object.keys(req.files).length === 0)
+    return next(new HttpError('Please provide a file', 404));
+  //@ts-ignore
+  const parser = await parse(req.files.file.data, {
+    trim: true,
+    skip_empty_lines: true,
+    delimiter: ':',
+  });
+  parser.on('readable', function () {
+    //
+  });
+
+  /**
+   * fileName : req.files.file.name
+   * fileContent : req.files.file.data : buffer representation of the file
+   */
+};
+
+export { getUploadedDocs, publishDataFromUploadedDoc };
