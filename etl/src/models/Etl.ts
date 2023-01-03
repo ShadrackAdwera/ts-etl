be727d4a1d4685f5ff0620d52fb88a4f5eb116e8
ShadrackAdwera <adweshshaddie@gmail.com>
2023-01-03 12:21:33 +0300

    set up etl models

--- etl/src/models/Etl.ts
+++ etl/src/models/Etl.ts
@@ -2,12 +2,14 @@ import { Document, Schema, model } from 'mongoose';
 
 export interface EtlDoc extends Document {
   createdBy: string;
+  docName: string;
   docUrl: string;
 }
 
 const etlSchema = new Schema<EtlDoc>(
   {
     createdBy: { type: String, required: true },
+    docName: { type: String, required: true },
     docUrl: { type: String, required: true },
   },
   { timestamps: true, toJSON: { getters: true } }
