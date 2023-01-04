e6e296b15ff3c215d15629131fff521fc238dd12
ShadrackAdwera <adweshshaddie@gmail.com>
2023-01-04 15:14:27 +0300

    set up data uploaded listener

---
+++ data/src/events/DataUploadedListener.ts
@@ -0,0 +1,31 @@
+import { Listener, CsvUploadedEvent, Subjects } from '@adwesh/v2-common';
+import { Message } from 'node-nats-streaming';
+
+// put this in the library
+enum PossibleOutcomes {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+export class DataUploadedListener extends Listener<CsvUploadedEvent> {
+  subject: Subjects = Subjects.CsvUploaded;
+  queueGroupName: string = 'DATA_QUEUE-GROUP';
+  async onMessage(
+    data: {
+      matchDay: Date;
+      homeTeam: string;
+      awayTeam: string;
+      homeScored: number;
+      awayScored: number;
+      winner: PossibleOutcomes;
+      ref: string;
+    },
+    msg: Message
+  ): Promise<void> {
+    const { homeTeam, awayTeam, homeScored, awayScored } = data;
+    console.log(`${homeTeam} ${homeScored} - ${awayTeam} ${awayScored}`);
+    msg.ack();
+    throw new Error('Method not implemented.');
+  }
+}
