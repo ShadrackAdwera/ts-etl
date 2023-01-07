2c26629489966485075b8607700a3d6f23ac2c1a
ShadrackAdwera <adweshshaddie@gmail.com>
2023-01-07 16:16:40 +0300

    configure listen to incoming requests into the data service

--- data/src/app.ts
+++ data/src/app.ts
@@ -1,11 +1,13 @@
 import { HttpError } from '@adwesh/common';
 import express, { Request, Response, NextFunction } from 'express';
+import { dataRouter } from './routes/data-routes';
 
 const app = express();
 
 app.use(express.json());
 
 //routes
+app.use('/data', dataRouter);
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   throw new HttpError('This method / path does ot exist', 404);
--- data/src/index.ts
+++ data/src/index.ts
@@ -1,8 +1,21 @@
-import mongoose from 'mongoose';
+import { pgPool } from './helpers/pool';
 
 import { app } from './app';
 import { natsWraper } from '@adwesh/common';
 import { DataUploadedListener } from './events/DataUploadedListener';
+
+if (!process.env.PGDATABASE) {
+  throw new Error('Database name must be defined');
+}
+if (!process.env.POSTGRES_PASSWORD) {
+  throw new Error('Database password must be defined');
+}
+if (!process.env.PGUSER) {
+  throw new Error('Database user must be defined');
+}
+if (!process.env.PGHOST) {
+  throw new Error('Database host must be defined');
+}
 
 if (!process.env.NATS_CLUSTER_ID) {
   throw new Error('NATS_CLUSTER_ID must be defined');
@@ -18,6 +31,29 @@ if (!process.env.NATS_URI) {
 
 const start = async () => {
   try {
+    await pgPool.connect({
+      database: process.env.PGDATABASE,
+      password: process.env.POSTGRES_PASSWORD,
+      user: process.env.PGUSER,
+      host: process.env.PGHOST,
+      port: 5432,
+    });
+
+    // TODO: FIX THIS - USE PG SCHEMA MIGRATIONS INSTEAD
+    pgPool._pool?.on('connect', (client) => {
+      client
+        .query(
+          `CREATE TABLE IF NOT EXISTS data 
+          (id SERIAL PRIMARY KEY, awayScored INT NOT NULL,homeScored INT NOT NULL,
+            homeTeam VARCHAR(20) NOT NULL,awayTeam VARCHAR(20) NOT NULL,
+            matchDay TIMESTAMP WITH TIME ZONE NOT NULL,referee VARCHAR(20) NOT NULL,
+            winner VARCHAR(10) NOT NULL,season INT NOT NULL);`
+        )
+        .catch((err) => {
+          throw err;
+        });
+    });
+
     await natsWraper.connect(
       process.env.NATS_CLUSTER_ID!,
       process.env.NATS_CLIENT_ID!,
@@ -34,7 +70,6 @@ const start = async () => {
 
     new DataUploadedListener(natsWraper.client).listen();
 
-    await mongoose.connect(process.env.MONGO_URI!);
     app.listen(5002);
     console.log('Connected to Data Service, listening on PORT: 5002');
   } catch (error) {
