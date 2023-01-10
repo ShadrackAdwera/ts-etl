6c3f12bd20d8dd50cf44e404eea2c5b29392c3ad
ShadrackAdwera <adweshshaddie@gmail.com>
2023-01-10 15:12:52 +0300

    publish successful auth events

--- auth/src/utils/passport.ts
+++ auth/src/utils/passport.ts
@@ -1,6 +1,12 @@
+import { natsWraper } from '@adwesh/common';
+import { Types } from 'mongoose';
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import { COOKIE_MAX_AGE } from '../app';
+import { AuthSuccessfulPublisher } from '../events/AuthSuccessfulPublisher';
 import { User, UserDoc } from '../models/User';
+
+type ResUser = UserDoc & { _id: Types.ObjectId };
 
 passport.serializeUser((user: any, done) => {
   done(undefined, user);
@@ -21,15 +27,26 @@ passport.use(
       proxy: true,
     },
     async (accessToken, refreshToken, profile, done) => {
+      let existingUser: ResUser | null;
       try {
-        const existingUser = await User.findOne({ googleId: profile.id });
+        existingUser = await User.findOne({ googleId: profile.id });
         if (existingUser) {
+          //
+          await new AuthSuccessfulPublisher(natsWraper.client).publish({
+            userId: existingUser._id.toString(),
+            ttl: COOKIE_MAX_AGE,
+          });
           return done(undefined, existingUser);
         }
         const user = await new User({
           googleId: profile.id,
           username: profile.displayName,
         }).save();
+        //
+        await new AuthSuccessfulPublisher(natsWraper.client).publish({
+          userId: user._id.toString(),
+          ttl: COOKIE_MAX_AGE,
+        });
         done(null, user);
       } catch (err) {
         done(err instanceof Error ? err : 'An error occured', undefined);
