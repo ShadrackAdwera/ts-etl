651a5e733e58f4b6d3a8aef4150af6ad906dd62a
ShadrackAdwera <adweshshaddie@gmail.com>
2023-01-10 15:10:19 +0300

    listen to auth successful events

---
+++ data/src/events/AuthSuccessfulListener.ts
@@ -0,0 +1,23 @@
+import { initRedis } from '@adwesh/common';
+import { Listener, AuthSuccessfulEvent, Subjects } from '@adwesh/v2-common';
+import { Message } from 'node-nats-streaming';
+
+export class AuthSuccessfulListener extends Listener<AuthSuccessfulEvent> {
+  subject: Subjects.AuthSuccessful = Subjects.AuthSuccessful;
+  queueGroupName: string = 'DATA_QUEUE-GROUP';
+  async onMessage(
+    data: { userId: string; ttl: number },
+    msg: Message
+  ): Promise<void> {
+    try {
+      await initRedis.client.set(data.userId, data.userId, {
+        EX: data.ttl / 1000,
+      });
+      msg.ack();
+    } catch (error) {
+      throw new Error(
+        error instanceof Error ? error.message : 'Method not implemented.'
+      );
+    }
+  }
+}
