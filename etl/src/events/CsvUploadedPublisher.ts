11eba9b9986da2ec54ca2004974a1cb3cbe33b1c
ShadrackAdwera <adweshshaddie@gmail.com>
2023-01-10 15:10:54 +0300

    subjects fix

--- etl/src/events/CsvUploadedPublisher.ts
+++ etl/src/events/CsvUploadedPublisher.ts
@@ -1,5 +1,5 @@
 import { CsvUploadedEvent, Subjects, Publisher } from '@adwesh/v2-common';
 
 export class CsvUploadedPublisher extends Publisher<CsvUploadedEvent> {
-  subject: Subjects = Subjects.CsvUploaded;
+  subject: Subjects.CsvUploaded = Subjects.CsvUploaded;
 }
