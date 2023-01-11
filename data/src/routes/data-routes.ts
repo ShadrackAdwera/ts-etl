27c788818395ae43fc7ab9b32ac8044256944754
ShadrackAdwera <adweshshaddie@gmail.com>
2023-01-11 06:00:46 +0300

    set up data routes

--- data/src/routes/data-routes.ts
+++ data/src/routes/data-routes.ts
@@ -1,11 +1,19 @@
 import { Router } from 'express';
+
 import { checkAuth } from '../middlewares/checkAuth';
 
-import { fetchMatches } from './../controllers/data-controllers';
+import {
+  fetchMatches,
+  addMatch,
+  deleteMatch,
+  updateMatch,
+} from './../controllers/data-controllers';
 
 const router = Router();
 
-// add auth middleware
 router.get('', checkAuth, fetchMatches);
+router.post('', [], addMatch);
+router.patch('/:id', [], updateMatch);
+router.delete(':/id', [], deleteMatch);
 
 export { router as dataRouter };
