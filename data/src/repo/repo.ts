d8a986df9ec861ccb5d3ceb2761dabfc5019fcec
ShadrackAdwera <adweshshaddie@gmail.com>
2023-01-11 05:50:53 +0300

    modify insert return type

--- data/src/repo/repo.ts
+++ data/src/repo/repo.ts
@@ -2,7 +2,7 @@ import { pgPool } from './../helpers/pool';
 import { StatType } from './../helpers/types';
 
 export class DataRepo {
-  static async insert(data: StatType): Promise<StatType[]> {
+  static async insert(data: StatType): Promise<StatType> {
     const {
       awayScored,
       awayTeam,
@@ -15,7 +15,7 @@ export class DataRepo {
     } = data;
     const { rows } = await pgPool.query<StatType>(
       `INSERT INTO data (awayScored,homeScored, homeTeam, awayTeam, matchDay, referee, winner, season)
-        VALUES ($1,$2,$3,$4,$5,$6,$7,$8)`,
+        VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *`,
       [
         awayScored,
         homeScored,
@@ -27,7 +27,7 @@ export class DataRepo {
         season,
       ]
     );
-    return rows;
+    return rows[0];
   }
 
   static async getAll(): Promise<StatType[]> {
