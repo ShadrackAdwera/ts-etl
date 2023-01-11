ad4cd80d062e97ec3d320b253c3be3de25fbad71
ShadrackAdwera <adweshshaddie@gmail.com>
2023-01-11 05:51:08 +0300

    configure add match controller

--- data/src/controllers/data-controllers.ts
+++ data/src/controllers/data-controllers.ts
@@ -23,4 +23,42 @@ const fetchMatches = async (
   res.status(200).json({ count: stats.length, stats });
 };
 
+const addMatch = async (req: Request, res: Response, next: NextFunction) => {
+  let response: StatType;
+  // TODO: Add created by mongo id
+  const {
+    homeTeam,
+    awayTeam,
+    homeScored,
+    awayScored,
+    matchDay,
+    ref,
+    winner,
+    season,
+  }: StatType = req.body;
+
+  try {
+    response = await DataRepo.insert({
+      awayScored,
+      awayTeam,
+      homeScored,
+      homeTeam,
+      matchDay,
+      ref,
+      season,
+      winner,
+    });
+  } catch (error) {
+    return next(
+      new HttpError(
+        error instanceof Error
+          ? error.message
+          : 'An error occured inserting data, try again',
+        500
+      )
+    );
+  }
+  res.status(201).json({ message: 'Insert successful', data: response });
+};
+
 export { fetchMatches };
